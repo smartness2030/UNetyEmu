21a1f8473df129d8f6f4ee171f36e9d240d147d7
mauricio6519 <m272321@dac.unicamp.br>
2026-03-27 15:09:40 +0100

    Update in the code comments

--- UNetyEmuROS/Assets/Scripts/Components/Car/Inputs/MoveCarKeyboard.cs
+++ UNetyEmuROS/Assets/Scripts/Components/Car/Inputs/MoveCarKeyboard.cs
@@ -1,5 +1,5 @@
 // ----------------------------------------------------------------------
-// Copyright 2025 INTRIG & SMARTNESS
+// Copyright 2026 INTRIG & SMARTNESS
 // Licensed under Apache 2.0: http://www.apache.org/licenses/LICENSE-2.0
 // ----------------------------------------------------------------------
 
